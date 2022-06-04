660eea02228173a9bc65df27f6fc25cfe8e9d1f6
Danny Lin <danny.l@ljit.io>
2022-06-04 17:28:06 +0800

    Update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,23 @@
 let js = 'amazing';
+let firstName = 'Jonas';
+let jonas_matilda = 'JM';
+let $funtion = 27;
+let person = 'jonas';
 
-if (js === 'amazing') {
-	alert('JavaScript is fun');
-}
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+// Don't write code like this!
+let job1 = 'Programmer';
+let job2 = 'Teacher';
+
+// 不變的常數都用大寫表示
+const PI = 3.1415;
 
 console.log(40 + 8 +23 - 10);
+console.log('Jonas');
+console.log(23);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(myFirstJob);
