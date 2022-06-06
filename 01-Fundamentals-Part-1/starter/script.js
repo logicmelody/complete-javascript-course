f155acb8bc0685a2e32cc40bd1f8b9fa56a0084f
Danny Lin <danny.l@ljit.io>
2022-06-06 20:04:06 +0800

    Update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 let firstName = 'Jonas';
 let jonas_matilda = 'JM';
@@ -14,10 +15,52 @@ let job2 = 'Teacher';
 // 不變的常數都用大寫表示
 const PI = 3.1415;
 
-console.log(40 + 8 +23 - 10);
+console.log(40 + 8 + 23 - 10);
 console.log('Jonas');
 console.log(23);
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 console.log(myFirstJob);
+*/
+
+// Primitive data types
+// Number
+let age = 23;
+
+// String
+let firstName = 'Jonas';
+
+// Boolean
+let javascriptIsFun = true;
+
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES';
+
+console.log(typeof javascriptIsFun);
+
+// Undefined
+let children;
+let year;
+
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+
+console.log(typeof year);
+
+// 這行會 return object，應該是要 return null，可能是 Javascript 的 bug 或 error
+console.log(typeof null);
+
+// Null
+
+// Symbol (ES2015)
+
+// BigInt (ES2020)
