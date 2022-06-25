e3461797b891aa47f09d5bcf36ffb18678f5a6e5
Danny Lin <danny.l@ljit.io>
2022-06-25 14:58:27 +0800

    Update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -85,3 +85,52 @@ lastName = 'Lin';
 console.log(lastName);
 */
 
+/*
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+
+console.log(ageJonas, ageSarah);
+
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 = 8
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // x = 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+x--; // x = x - 1 = 99
+
+console.log(x);
+
+// Comparision operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2020);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+
+console.log(now - 1991 > now - 2020);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+
+console.log(ageJonas, ageSarah, averageAge);
