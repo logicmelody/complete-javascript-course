97c936d361bc1609a2e5a32fc3cda2a98c23d211
Danny Lin <danny.l@ljit.io>
2022-06-27 20:35:34 +0800

    Update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -159,6 +159,7 @@ multiple
 lines`);
 */
 
+/*
 const age = 15;
 
 // command + control + space，可以加 emoji
@@ -180,3 +181,57 @@ if (birthYear <= 2000) {
 }
 
 console.log(century);
+*/
+
+/*
+// Type conversion
+const inputYear = '1991';
+
+// Javascript 可以把值轉換成 number, string, boolean
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// Type coercion
+console.log('I am ' + 23 + ' years old'); // "string + xxx" => 這種會被轉成 string
+console.log('23' - '10' - 3, '23' + '10' + 3); // + 以外會被轉換成 number
+console.log('23' * '2', '23' / '2');
+
+let n = '1' + 1;
+n = n - 1;
+
+console.log(n); // 10
+console.log(2 + 3 + 4 + '5'); // 95
+console.log('10' - '4' - '3' - 2 + '5'); // 15
+*/
+
+// 5 falsy values: 0, '', undefined, null, NaN
+// falsy values: 經過 type coercion，會被轉換成 false 的 boolean
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+
+if (money) {
+	console.log("Don't spend it all");
+
+} else {
+	console.log("You should get a job");
+}
+
+let height = 100;
+
+if (height) {
+	console.log("Height is defined");
+
+} else {
+	console.log("Height is UNDEFINED");
+}
