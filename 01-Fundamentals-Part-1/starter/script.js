2c69fcc3dc108b25c0d4cf5bc65ee04003cb9421
Danny Lin <danny.l@ljit.io>
2022-07-17 18:29:20 +0800

    Update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -274,6 +274,7 @@ if (favorite !== 23) {
 }
 */
 
+/*
 const hasDrviersLicense = true; // A
 const hasGoodVision = true; // B
 
@@ -298,4 +299,55 @@ if (hasDrviersLicense && hasGoodVision && !isTired) {
 } else {
 	console.log('Someone else should drive...');
 }
+*/
 
+const day = 'friday';
+
+switch (day) {
+	case 'monday': // day === 'monday'
+		console.log('Plan course structure');
+		console.log('Go to coding meetup');
+		break;
+
+	case 'tuesday':
+		console.log('Prepare theory videos');
+		break;
+
+	case 'wednesday':
+	case 'thursday':
+		console.log('Write code examples');
+		break;
+
+	case 'friday':
+		console.log('Record videos');
+		break;
+
+	case 'saturday':
+	case 'sunday':
+		console.log('Enjoy the weekend');
+		break;
+
+	default:
+		console.log('Not a valid day');
+		break;
+}
+
+if (day === 'monday') {
+	console.log('Plan course structure');
+	console.log('Go to coding meetup');
+
+} else if (day === 'tuesday') {
+	console.log('Prepare theory videos');
+
+} else if (day === 'wednesday' || day === 'thursday') {
+	console.log('Write code examples');
+
+} else if (day === 'friday') {
+	console.log('Record videos');
+
+} else if (day === 'saturday' || day === 'sunday') {
+	console.log('Enjoy the weekend');
+
+} else {
+	console.log('Not a valid day');
+}
