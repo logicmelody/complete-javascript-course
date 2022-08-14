c60111b6cfdf8924b776f45a8c282a7ad37a2eba
Danny Lin <danny.l@ljit.io>
2022-08-14 15:02:58 +0800

    Update

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -354,8 +354,17 @@ const jonas = [
 	2037 - 1991,
 	'teacher',
 	['Michael', 'Peter', 'Steven'],
+	true,
 ];
 
 for (let i = jonas.length - 1; i >= 0; i--) {
 	console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+	console.log(`Starting exercise ${exercise}`);
+
+	for (let rep = 1; rep < 6; rep++) {
+		console.log(`Exercise ${exercise}: Repetition ${rep}`);
+	}
 }
