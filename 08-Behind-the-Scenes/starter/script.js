a2342c6baff4a146bc9b08a502c506ef10d88997
Danny Lin <danny.l@ljit.io>
2022-10-07 15:21:08 +0800

    Update

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -40,3 +40,85 @@ function calcAge(birthYear) {
 const firstName = 'Jonas';
 
 calcAge(1991);
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+	return a + b;
+}
+
+const addExpr = function (a, b) {
+	return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+
+if (!numProducts) {
+	deleteShoppingCart();
+}
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+	console.log('All products deleted');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
+
+console.log(this);
+
+const calaAge = function (birthYear) {
+	console.log(2037 - birthYear);
+	console.log(this);
+}
+
+const calaAgeArrow = (birthYear) => {
+	console.log(2037 - birthYear);
+	console.log(this);
+}
+
+calaAge(1991);
+calaAgeArrow(1980);
+
+const jonas = {
+	year: 1991,
+	calaAge: function () {
+		console.log(this);
+		console.log(2037 - this.year);
+	},
+};
+
+jonas.calaAge();
+
+const matilda = {
+	year: 2017,
+};
+
+matilda.calaAge = jonas.calaAge;
+matilda.calaAge();
+
+const f = jonas.calaAge;
+
+f();
